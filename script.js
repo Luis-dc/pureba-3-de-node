6c702cb23ac0c97c9f974308024d90afc0063e7c
Luis-dc <jdomingoc1@miumg.edu.gt>
2024-10-01 20:41:26 -0600

    primer commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Hellor world with express'))
+
+app.listen(3000);
+console.log('server on port 3000')
